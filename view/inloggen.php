459ae9055cfb019834f87b9507bcd1d621c72119
TeunJansen <teunjansen15@hotmail.com>
2020-12-03 12:28:12 +0100

    geupdate

--- view/inloggen.php
+++ view/inloggen.php
@@ -5,34 +5,20 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <div id="frm">
-        <p>
-            <label>Username:</label>
-            <input type="text" id="user" name="user"/>
-        </p>
-        <p>
-            <label>Password:</label>
-            <input type="password" id="pass" name="pass"/>
-        </p>
-        <p>
-            <input type="submit" id="btn" value="login"/>
-        </p>
-    </div>
+
 
     <div class="header">
         <h2>Register</h2>
     </div>
-<<<<<<< HEAD
     
     <form method="post" action="inloggen">
         <!-- display errors here -->
         <?php include('errors.php'); ?>
         
-=======
+
 
     <form method="post" action="inloggin.php">
 
->>>>>>> d05c25a01abb3b8a68057ba5fa630b7326a8015c
         <div class="input-group">
             <label>Username:</label>
             <input type="text" name="username">
@@ -58,12 +44,11 @@
         </div>
 
         <p>
-<<<<<<< HEAD
+
          Already a member? <a href="login">Log in</a>
-=======
-            <input type="submit" id="btn" value="login"/>
-            Already a member? <a href="login.php">Log in</a>
->>>>>>> d05c25a01abb3b8a68057ba5fa630b7326a8015c
+
+          
+            
         </p>
         </div>
 
--- view/login.php
+++ view/login.php
@@ -5,7 +5,6 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<<<<<<< HEAD
     <div class="header">
         <h2>Login</h2>
     </div>
@@ -13,22 +12,16 @@
     <form method="post" action="login">
         
         <div class="input-group">
-=======
-<div class="header">
-    <h2>Login</h2>
-</div>
 
 <form method="post" action="login.php">
 
     <div class="input-group">
->>>>>>> d05c25a01abb3b8a68057ba5fa630b7326a8015c
         <label>Username:</label>
         <input type="text" name="username">
     </div>
 
     <div class="input-group">
         <label>Password:</label>
-<<<<<<< HEAD
         <input type="password" name="password_1"> 
         </div>
         
@@ -40,27 +33,8 @@
          Not yet a member? <a href="inloggen">Sign up</a>
         </p>
             
-    </form>
-    
-    
-    
-    </body>
-=======
-        <input type="password" name="password_1">
-    </div>
-
-    <div class="input_group">
-        <button type="submit" name="login" class="btn">Login</button>
-    </div>
-
-    <p>
-        Not yet a member? <a href="inloggen.php">Sign up</a>
-    </p>
-
-</form>
-
+            </form>
 
 
 </body>
->>>>>>> d05c25a01abb3b8a68057ba5fa630b7326a8015c
 </html>
