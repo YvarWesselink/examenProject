55213c3393bfbfb594e5e285bd80f06ebb072bd7
YvarWesselink <70689662+YvarWesselink@users.noreply.github.com>
2020-12-17 11:49:20 +0100

    Routes userlevl
    added user level route

--- routes.php
+++ routes.php
@@ -68,3 +68,7 @@ Route::set('acgegevens', function () {
 Route::set('logout', function () {
     Admin::logout();
 });
+
+Route::set('userlevel', function () {
+    Index::CreateView('userlevel');
+});
