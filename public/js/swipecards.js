fc203715f74caf40844c64a2c945c35b0e23bac8
SamVelthof <svelthof0596@student.landstede.nl>
2021-01-19 12:29:03 +0100

    update

--- public/js/swipecards.js
+++ public/js/swipecards.js
@@ -1,6 +1,8 @@
-$(document).ready(function(){
-    $('.image-slide').slick({
+$(document).ready(function () {
+    $('#lekker-sliden').slick({
         slidesToShow: 1,
-
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 15000,
     })
 });
