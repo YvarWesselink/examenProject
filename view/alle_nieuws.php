770f564546a9aff6c3545ccf34c45baec2ba6a98
martvredeveld <martvredevekd@gmail.com>
2021-01-27 12:02:04 +0100

    Update alle_nieuws.php

--- view/alle_nieuws.php
+++ view/alle_nieuws.php
@@ -7,5 +7,20 @@
 </head>
 <body>
 
+<?php
+$conn = database::connect();
+$sql = "SELECT userID, Name, Email, Comments FROM feedback";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo '<div class="nieuws"><td><br>' . $row["userID"]. "</td><td><br>" . $row["Name"] . "</td><td><br>"
+. $row["Email"]. "</td><td><br>" . $row["Comments"]. "</td><br><br></div>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+
 </body>
 </html>
