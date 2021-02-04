29733e95cd6f3ee3ec5fa81f9ec285442d9dc1d3
martvredeveld <martvredevekd@gmail.com>
2021-02-04 09:58:05 +0100

    Update excersises.php

--- controller/excersises.php
+++ controller/excersises.php
@@ -121,10 +121,7 @@ class excersises extends Database {
                 $value = $_POST;
                 $key = $tableNames[$i]['colomn'];
 
-                $to = "yourname@yourwebsite.com";
-                $headers = "From: $email_from \r\n";
-                $headers .= "Reply-To: $visitor_email \r\n";
-                mail($to,$email_subject,$email_body,$headers);
+                
 
                 $backLog = $value[$key];
             } else {
