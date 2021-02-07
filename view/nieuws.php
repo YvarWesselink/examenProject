5d9aa4da0af3fa0dacac3f9b3d4b804730cc3c00
AMMAR-2\Ammar <aaljwab8539@student.landstede.nl>
2021-02-07 16:28:21 +0100

    CK editor toegevoegd aan nieuws pagina

--- view/nieuws.php
+++ view/nieuws.php
@@ -6,9 +6,41 @@ include_once "includes/header.php";
 
 $conn = self::connect();
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$result = $conn->query("SELECT * FROM images");
+$images = $conn->query("SELECT * FROM images");
+$albums = $conn->query("SELECT * FROM albums");
+
+if(isset($_GET['action']) == "albumsNaam"){
+
+    echo 'goed getest';
+
+}else{
+    
+    echo 'Error met images ophalen uit Ajax';
+}
 
 ?>
+<script src="//cdn.ckeditor.com/4.16.0/full/ckeditor.js"></script>
+<style>
+
+    label input{
+        padding: 10px 15px !important;
+        margin: 6px 4px !important;
+        background: transparent !important;
+        border: 2px solid #ddd !important;
+        font-weight: bold !important;
+        cursor: pointer !important;
+        border-radius: 3px !important;
+        display: inline-block !important;
+        width: auto !important;
+    }
+
+    label input:focus,
+    label input:active{
+        outline: none;
+        border-color: #f4b40e !important;
+    }
+
+</style>
 
 <div class="txthome-container">
     <div>
@@ -30,26 +62,73 @@ $result = $conn->query("SELECT * FROM images");
         <label>Bedrijf</label>
         <input type="text" name="Company" autocomplete="off" />
         <label>Bericht</label>
-        <input type="text" name="Comments" autocomplete="off"/>
+        <textarea name="editor1"></textarea>
+        <script>
+                CKEDITOR.replace( 'editor1' );
+        </script>
+        <br>
+        <label style="display: block;">
+            <span style="display: block; margin-bottom: 15px;">Kies album:</span>
+            <?php
+
+            if ($albums->rowCount() > 0){
+                $row = $albums->fetchAll(PDO::FETCH_ASSOC);
+                $count = count($row);
+                $a = 0;
+
+                while($count > $a){
+
+                    echo '<input type="button" class="albumsNaam" value="' . $row[$a]['naam'] . '">';
+                    $a++;
+                    
+                }
+                
+            } else {
+
+                    echo "<select name='foto'>Er zijn geen foto's om te kiezen!</select><br>";
+                
+            }
+
+            ?>
+        </label>
         <!-- <form method="post" action="" enctype='multipart/form-data' class="upload-image"> -->
-        <label>Nieuws foto</label>
+        <label>Nieuws foto:</label>
         <div class="images" style="text-align: left; display: block;">
             <?php
 
-                if ($result->rowCount() > 0){
-                    $row = $result->fetchAll(PDO::FETCH_ASSOC);
+                if ($images->rowCount() > 0){
+                    $row = $images->fetchAll(PDO::FETCH_ASSOC);
                     $count = count($row);
                     $i = 0;
                     $d = 0;
                     
                     while($count > $i){
-                        echo "<div style='width: 85px; height: auto; position: relative; overflow: hidden; margin: 0 5px 15px; display: inline-block;'><img src='". $row[$i]['image'] ."' style='width: 100%; height: 85px;border-radius: 3px;'/><span style='display: block; text-align: center;'>" . $row[$i]['id'] . "</span></div>";
+                        echo "<div style='width: 85px; height: auto; position: relative; overflow: hidden; margin: 0 5px 15px; display: inline-block;'>
+                                <img src='". $row[$i]['image'] ."' style='width: 100%; height: 85px;border-radius: 3px;'/>
+                                <span style='display: block; text-align: center;'>" . $row[$i]['id'] . "</span>
+                            </div>";
                         $i++;
                         }
 
                         ?>
 
-            <label>Kies foto
+                        <script>
+                            $('.albumsNaam').click(function(){
+                                $.ajax({
+                                    url: 'http://192.168.2.19/nieuws',
+                                    type: "GET",
+                                    dataType: 'JSON',
+                                    success: function(albumsNaam){
+                                        alert('done');
+                                    },
+                                    error: function(a,b,c){
+                                        alert(b.responseText);
+                                    }
+                                });
+                            });
+                        </script>
+
+            <label>Kies foto:
                 <select name="foto">
                     <option value="0">Geen foto</option>
                     <?php
@@ -74,7 +153,7 @@ $result = $conn->query("SELECT * FROM images");
             </label>
         </div>
         <label style="display: block;">
-            Kies School
+            Kies School:
             <select name="school">
                 <option value="z">Zwolle</option>
                 <option value="s">Salland</option>
