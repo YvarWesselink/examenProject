aa35b9f146a91965f3cb7918a7763ff12e37cb4f
AMMAR-2\Ammar <aaljwab8539@student.landstede.nl>
2021-04-06 14:06:38 +0200

    Fixen van nieuws-weergeven

--- view/salland.php
+++ view/salland.php
@@ -85,18 +85,19 @@ echo "<h2>".$salland['titels']."</h2>"."<h3>".$salland['tussens']."</h3>"."<p>".
 <!-- Start Nieuws Section -->
 
 <?php
-$school = $_SESSION['school'];
-$nieuwsContent = Admin::downloadNieuwsHome($school);
+    $school = $_SESSION['school'];
+    $nieuwsContent = Admin::downloadNieuwsHome($school);
 
-if (count($nieuwsContent) == 0) {
-    $nieuwsContent = null;
-}
+    if (count($nieuwsContent) == 0) {
+        $nieuwsContent = null;
+    }
+    
 ?>
 
 <section class="nieuws">
     <h2>Nieuws</h2>
     <div class="timeline">
-    <div class="all-content">
+        <div class="all-content">
             <div class="nieuws-container left">
                 <span class="icon"></span>
                 <span class="date">07 NOVEMBER 2017</span>
@@ -106,10 +107,10 @@ if (count($nieuwsContent) == 0) {
                         if (isset($nieuwsContent[0]['Comments'])) {
 
                             $nieuwsCon = $nieuwsContent[0]['Comments'];
-                            echo '<h2>' . $nieuwsCon . '</h2>';
-                            echo '<a href="#">Lees meer <i class="fas fa-arrow-right icons"></i></a>';
+                            echo '<h2>' . $nieuwsContent[0]['Name'] . '</h2>';
+                            echo '<form method="POST" action="/nieuws-artikel"><a type="submit" name="userID" href="/nieuws-artikel?artikel=' . $nieuwsContent[1]['userID'] . '">Lees meer <i class="fas fa-arrow-right icons"></i></a></form>';
                         
-                        }elseif(isset($nieuwsContent[0]['foto']) == 'Geen' || empty($nieuwsContent[0]['foto'])) {
+                        }else {
                             
                             echo "<h2 style='margin-bottom: 28px;'>Nog geen nieuws toegevoegd!</h2>";
                         }
@@ -118,27 +119,37 @@ if (count($nieuwsContent) == 0) {
                 </div>
             </div>
             <div class="image">
-                
                 <?php
 
                     if(isset($nieuwsContent[0]['foto'])){
 
                         $nieuwsFoto = $nieuwsContent[0]['foto'];
-                        
+
+                        // get album from selected image
+                        $pdo = Database::connect();
+                        $st = $pdo->prepare("SELECT album FROM images WHERE image=:image");
+                        $st->bindParam(":image", $nieuwsFoto);
+                        $st->execute();
+                        $album = $st->fetch(PDO::FETCH_ASSOC);
+                        $album = $album['album'];
                     }
 
                     if (isset($nieuwsContent[0]['foto']) && $nieuwsFoto !== '0') {
 
                         $foto = $nieuwsContent[0]['foto'];
-                        echo "<a href='/fotos'><img src='$foto' alt=''/></a>";
+                        echo "<form method='POST' action='/nieuws-artikel'><a type'submit'  name='userID' href='/nieuws-artikel?artikel=" . $nieuwsContent[1]['userID'] ."'><img src='$foto' alt=''/></a></form>";                    
 
+                        echo "<form method='post' action='/album-weergeven-school'>";
+                        echo "<input class='nieuws-image' type='image' src='$foto' alt='image'>";
+                        echo "<input type='hidden' name='album-nieuws' value='$album'>";
+                        echo "</form>";
+                    
                     }else {
                         
                         echo "<p style='text-decoration: underline;'>Nog geen foto!</p>";
                     }
 
                 ?>
-
             </div>
         </div>
         <div class="all-content">
@@ -151,10 +162,10 @@ if (count($nieuwsContent) == 0) {
                         if (isset($nieuwsContent[1]['Comments'])) {
 
                             $nieuwsCon = $nieuwsContent[1]['Comments'];
-                            echo '<h2>' . $nieuwsCon . '</h2>';
-                            echo '<a href="#">Lees meer <i class="fas fa-arrow-right icons"></i></a>';
+                            echo '<h2>' . $nieuwsContent[1]['Name'] . '</h2>';
+                            echo '<form method="POST" action="/nieuws-artikel"><a type="submit" name="userID" href="/nieuws-artikel?artikel=' . $nieuwsContent[2]['userID'] . '">Lees meer <i class="fas fa-arrow-right icons"></i></a></form>';
                         
-                        }elseif(isset($nieuwsContent[1]['foto']) == 'Geen' || empty($nieuwsContent[1]['foto'])) {
+                        }else {
                             
                             echo "<h2 style='margin-bottom: 28px;'>Nog geen nieuws toegevoegd!</h2>";
                         }
@@ -163,27 +174,39 @@ if (count($nieuwsContent) == 0) {
                 </div>
             </div>
             <div class="image img-left">
-                
                 <?php
 
                     if(isset($nieuwsContent[1]['foto'])){
 
                         $nieuwsFoto = $nieuwsContent[1]['foto'];
+
+                        // get album from selected image
+                        $pdo = Database::connect();
+                        $st = $pdo->prepare("SELECT album FROM images WHERE image=:image");
+                        $st->bindParam(":image", $nieuwsFoto);
+                        $st->execute();
+                        $album = $st->fetch(PDO::FETCH_ASSOC);
+                        $album = $album['album'];
                         
                     }
-
+                    
                     if (isset($nieuwsContent[1]['foto']) && $nieuwsFoto !== '0') {
 
                         $foto = $nieuwsContent[1]['foto'];
-                        echo "<a href='/fotos'><img src='$foto' alt=''/></a>";
-
-                    }else {
+                        echo "<form method='POST' action='/nieuws-artikel'><a type='submit' name='userID' href='/nieuws-artikel?artikel=" . $nieuwsContent[2]['userID'] ."'><img src='$foto' alt=''/></a></form>";                    
+                        echo "<form method='post' action='/album-weergeven-school'>";
+                        echo "<input class='nieuws-image' type='image' src='$foto' alt='image'>";
+                        echo "<input type='hidden' name='album-nieuws' value='$album'>";
+                        echo "</form>";
+                    
+                        $nieuwsFoto = $nieuwsContent[1]['foto'];
+                    
+                    } else {
                         
                         echo "<p style='text-decoration: underline;'>Nog geen foto!</p>";
                     }
-
+                
                 ?>
-
             </div>
         </div>
         <div class="all-content">
@@ -196,51 +219,56 @@ if (count($nieuwsContent) == 0) {
                         if (isset($nieuwsContent[2]['Comments'])) {
 
                             $nieuwsCon = $nieuwsContent[2]['Comments'];
-                            echo '<h2>' . $nieuwsCon . '</h2>';
-                            echo '<a href="#">Lees meer <i class="fas fa-arrow-right icons"></i></a>';
+                            echo '<h2>' . $nieuwsContent[2]['Name'] . '</h2>';
+                            echo '<form method="POST" action="/nieuws-artikel"><a type="submit" name="userID" href="/nieuws-artikel?artikel=' . $nieuwsContent[3]['userID'] . '">Lees meer <i class="fas fa-arrow-right icons"></i></a></form>';
                         
-                        }elseif(isset($nieuwsContent[2]['foto']) == 'Geen' || empty($nieuwsContent[2]['foto'])) {
+                        }else {
                             
                             echo "<h2 style='margin-bottom: 28px;'>Nog geen nieuws toegevoegd!</h2>";
                         }
-                    
+                
                     ?>
                 </div>
             </div>
             <div class="image">
-                
                 <?php
 
                     if(isset($nieuwsContent[2]['foto'])){
 
                         $nieuwsFoto = $nieuwsContent[2]['foto'];
-                        
-                    }
 
+                    }
+                                            
                     if (isset($nieuwsContent[2]['foto']) && $nieuwsFoto !== '0') {
 
                         $foto = $nieuwsContent[2]['foto'];
-                        echo "<a href='/fotos'><img src='$foto' alt=''/></a>";
 
+                        echo "<form method='POST' action='/nieuws-artikel'><a type='submit' name='userID' href='/nieuws-artikel?artikel=" . $nieuwsContent[3]['userID'] ."'><img src='$foto' alt=''/></a></form>";
+
+                        echo "<form method='post' action='/album-weergeven-school'>";
+                        echo "<input class='nieuws-image' type='image' src='$foto' alt='image'>";
+                        echo "<input type='hidden' name='album-nieuws' value='$album'>";
+                        echo "</form>";
+                    
+                        $nieuwsFoto = $nieuwsContent[2]['foto'];
+                    
                     }else {
                         
                         echo "<p style='text-decoration: underline;'>Nog geen foto!</p>";
                     }
-
+                    
                 ?>
-
             </div>
         </div>
     </div>
     <?php
 
-        if(count($nieuwsContent) > 3){
-                    
-            echo '<form method="POST" action="/nieuws-weergeven"><button type="submit" name="nieuws-weergeven-salland" class="kijk_meer">Kijk meer <i class="fas fa-arrow-right icons"></i></button></form>';
-
+        if (count($nieuwsContent) > 3) {
+            echo '<form method="POST" action="/nieuws-weergeven"><button type="submit" name="nieuws-weergeven-zwolle" class="kijk_meer">Kijk meer <i class="fas fa-arrow-right icons"></i></button></form>';
         }
 
     ?>
+    
 </section>
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f9f9f9" fill-opacity="1" d="M0,192L120,176C240,160,480,128,720,122.7C960,117,1200,139,1320,149.3L1440,160L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg>
 <div class="clearfix"></div>
